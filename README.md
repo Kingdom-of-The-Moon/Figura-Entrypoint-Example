5ff377b81b205aacf709e3bb8f96ccf177807662
Fran <40085587+Francy-chan@users.noreply.github.com>
2021-12-04 14:45:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Figura Entrypoint Example
+* Example mod for custom Lua APIs in Figura
---
+++ src/main/java/net/blancworks/figura_entrypoint_example/ExampleAPI.java
@@ -0,0 +1,30 @@
+package net.blancworks.figura_entrypoint_example;
+
+import net.blancworks.figura.lua.CustomScript;
+import net.blancworks.figura.lua.api.FiguraAPI;
+import net.blancworks.figura.lua.api.ReadOnlyLuaTable;
+import net.minecraft.util.Identifier;
+import org.luaj.vm2.LuaTable;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.ZeroArgFunction;
+
+public class ExampleAPI implements FiguraAPI {
+    @Override
+    public Identifier getID() {
+        return new Identifier("example", "test_api");
+    }
+
+    @Override
+    public LuaTable getForScript(CustomScript customScript) {
+        return new ReadOnlyLuaTable(new LuaTable() {{
+
+            set("test", new ZeroArgFunction() {
+                @Override
+                public LuaValue call() {
+                    return LuaValue.valueOf("Custom API /// TEST");
+                }
+            });
+
+        }});
+    }
+}
